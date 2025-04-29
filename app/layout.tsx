0754d8d979da5bb20dec3eb8ef1c6d59b3201404
valdezfi <joswellv@outlook.com>
2025-04-28 22:59:54 -0400

    icons

--- app/layout.tsx
+++ app/layout.tsx
@@ -109,7 +109,7 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
       <head>
         <link rel="icon" href="/favicon.svg" type="image/svg+xml" />
         {/* <link rel="preconnect" href="https://fonts.googleapis.com" /> */}
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
+        {/* <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" /> */}
         {/* <link
           href="https://fonts.googleapis.com/css2?family=Inter+Tight:ital,wght@0,100..900;1,100..900&display=swap"
           rel="stylesheet"
