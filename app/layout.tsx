9f11aadcd446eb287321040e9aeff4597160fc84
valdezfi <joswellv@outlook.com>
2025-04-28 22:58:31 -0400

    golive

--- app/layout.tsx
+++ app/layout.tsx
@@ -110,10 +110,10 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
         <link rel="icon" href="/favicon.svg" type="image/svg+xml" />
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
-        <link
+        {/* <link
           href="https://fonts.googleapis.com/css2?family=Inter+Tight:ital,wght@0,100..900;1,100..900&display=swap"
           rel="stylesheet"
-        />
+        /> */}
 
         {/* Google Analytics script */}
         <script async src={`https://www.googletagmanager.com/gtag/js?id=G-335050025`}></script>
