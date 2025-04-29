2056f3b441f1095b937fc0bba47927a4f5171523
valdezfi <joswellv@outlook.com>
2025-04-28 23:05:59 -0400

    script

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { ThemeProvider } from "next-themes";
 import "styles/globals.css";
+import Script from 'next/script';
 
 export const metadata: Metadata = {
   title: "Numerobook | Instantly Buy & Sell Crypto",
@@ -108,16 +109,16 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
     <html lang="en" suppressHydrationWarning>
       <head>
         <link rel="icon" href="/favicon.svg" type="image/svg+xml" />
-        {/* <link rel="preconnect" href="https://fonts.googleapis.com" /> */}
-        {/* <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" /> */}
-        {/* <link
-          href="https://fonts.googleapis.com/css2?family=Inter+Tight:ital,wght@0,100..900;1,100..900&display=swap"
-          rel="stylesheet"
-        /> */}
+        {/* Optionally optimize fonts later with @next/font or preload */}
 
-        {/* Google Analytics script */}
-        <script async src={`https://www.googletagmanager.com/gtag/js?id=G-335050025`}></script>
-        <script
+        {/* Google Analytics (non-blocking) */}
+        <Script
+          src="https://www.googletagmanager.com/gtag/js?id=G-335050025"
+          strategy="afterInteractive"
+        />
+        <Script
+          id="ga-init"
+          strategy="afterInteractive"
           dangerouslySetInnerHTML={{
             __html: `
               window.dataLayer = window.dataLayer || [];
