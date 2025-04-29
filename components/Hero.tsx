54cf486a1352fe7529fff876f5a6cb527a86a293
valdezfi <joswellv@outlook.com>
2025-04-28 22:53:19 -0400

    fix

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -2,13 +2,12 @@ import Link from "next/link";
 
 export default function Hero() {
   return (
-    <section className="text-center my-32 mx-4 sm:mx-8 md:mx-16 lg:mx-24">
+    <section className="text-center my-32 mx-4 sm:mx-8 md:mx-16 lg:mx-24 bg-white dark:bg-black">
       <h1 className="font-sans text-3xl tracking-tighter sm:text-5xl md:text-6xl lg:text-7xl dark:text-white mb-6">
-      P2P Crypto Exchange
-
+        P2P Crypto Exchange
       </h1>
       <p className="text-xl text-gray-600 dark:text-gray-300 mb-12 font-light">
-      Buy crypto with many payment methods in our P2P exchange
+        Buy crypto with many payment methods in our P2P exchange
       </p>
       <div className="flex justify-center space-x-6">
         <Link
@@ -17,12 +16,6 @@ export default function Hero() {
         >
           Go To Market
         </Link>
-        {/* <Link
-          href="/supply"
-          className="bg-black text-white dark:bg-white dark:text-black px-5 py-2 rounded-md text-base font-semibold hover:bg-gray-800 dark:hover:bg-gray-200 transition duration-300"
-        >
-          Supply
-        </Link> */}
       </div>
     </section>
   );
