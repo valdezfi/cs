51e80be3f8a92b8c5fac7f019bdadd7d9c841dcd
valdezfi <joswellv@outlook.com>
2025-04-28 22:15:59 -0400

    update

--- app/_document.js.tsx
+++ app/_document.js.tsx
@@ -7,13 +7,12 @@ class MyDocument extends Document {
         <Head>
           {/* Preload font */}
           <link
-  rel="preload"
-  href="https://fonts.gstatic.com/s/intertight/v15/Q1cAV8F4kkalhpfvN-4obFy0gnoY5a_4Mw2rx2xuCMw.woff2"
-  as="font"
-  type="font/woff2"
-  crossOrigin="anonymous"
-/>
-
+            rel="preload"
+            href="/fonts/your-font.woff2"
+            as="font"
+            type="font/woff2"
+            crossOrigin="anonymous"
+          />
           {/* You can also preload additional fonts or resources here */}
         </Head>
         <body>
