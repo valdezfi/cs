050c88bbd1ad053986256d7fa3c1f54a5f2c2463
valdezfi <joswellv@outlook.com>
2025-07-12 17:14:10 -0400

    delete nb

--- app/allblogs/page.tsx
+++ app/allblogs/page.tsx
@@ -2,64 +2,9 @@ import Header from "components/Header";
 import BlogMultiCard from "components/BlogMultiPost";
 import Footer from "components/Footer";
 
-import { Metadata } from 'next';
 import Script from 'next/script';
 
-// SEO and social sharing metadata
-export const metadata: Metadata = {
-  title: "All Blogs",
-  description:
-    "All Crypto Blogs ",
-  keywords: [
- "crypto hedge fund",
-"crypto hedge",
-"crypto hedge fund fees",
-"crypto fund of funds",
-"crypto currency hedge fund",
-"crypto funds",
-"top crypto hedge funds",
-"crypto fund administrator",
-"crypto index fund",
-"bitcoin hedge fund",
-"crypto vc",
-"crypto asset fund",
-"blockchain vc",
-"blockchain fund",
-"momo",
-"bitcoin converter"
-  ],
-  openGraph: {
-    title: "All Blogs",
-    description:
-      "All Crypto Blogs",
-    url: "https://numerobook.com/allblogs",
-    images: [
-      {
-        url: "https://numerobook.com/images/OIG-19.jpg",
-        width: 1200,
-        height: 630,
-        alt: "All Crypto Blogs | Numerobook",
-      },
-    ],
-    siteName: "Numerobook",
-    locale: "en_US",
-    type: "website",
-  },
-  twitter: {
-    card: "summary_large_image",
-    title: " All Blogs| Grande",
-    description:
-      "All Grande Blogs",
-    images: ["https://numerobook.com/images/OIG-19.jpg"],
-  },
-  alternates: {
-    canonical: "https://numerobook.com/allblogs",
-  },
-  robots: {
-    index: true,
-    follow: true,
-  },
-};
+
 
 export default function Page() {
   return (
--- components/Header.tsx
+++ components/Header.tsx
@@ -102,7 +102,7 @@ export default function Header() {
             <li><Link href="/pricing" className="nav-link">Pricing</Link></li>
             <li>
               <Link
-                href="/signup"
+                href=""
                 className="bg-black text-white dark:bg-white dark:text-black px-5 py-2 rounded-md text-base font-semibold hover:bg-gray-800 dark:hover:bg-gray-200 transition duration-300"
               >
                 Signup/Login
