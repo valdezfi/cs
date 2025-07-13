afdb3cc5ce5edbf0028f97f0b08bb9f72c6d3612
valdezfi <joswellv@outlook.com>
2025-07-12 21:48:05 -0400

    fixed

--- app/brand/page.tsx
+++ app/brand/page.tsx
@@ -9,18 +9,10 @@ import Footer from "components/Footer";
 import BrandAccordion from "components/QABrand";
 import BrandHowItWorks from "components/BrandHIW";
 
-// import MexicoB from "components/Mexicobuy";
-// import Indiab from "components/IndiaBuy";
-// import Argentinabuynow from "components/Argentinabuy";
-// import UBuy from "components/Uruguaybuy";
 import USAb from "components/USAbuy";
-// import GHbuy from "components/GhanaBuy";
 import { Metadata } from 'next';
-import Script from 'next/script';
 import  BrandValueShowcase from "components/BrandResults";
-// import Creator from "components/ContentCreator"; // adjust the path based on your folder structure
 import PredictableHero from "components/Predictable"; // adjust the path based on your folder structure
-// import CreatorHowItWorks from "components/CreatorHIW"; // adjust the path based on your folder structure
 
 export const metadata: Metadata = {
   title: "Grande",
@@ -37,25 +29,6 @@ export default function Page() {
 
 
 
-<Script
-  id="twitter-meta-tags"
-  type="application/ld+json"
-  strategy="afterInteractive"
-  dangerouslySetInnerHTML={{
-    __html: JSON.stringify({
-      '@context': 'https://schema.org',
-      '@type': 'WebPage',
-      name: 'Buy Bitcoin with the Lowest Fees | Numerobook',
-      description:
-        'Numerobook makes crypto trading simple across the USA, LATAM, India, Africa, and beyond with local payment options and low fees.',
-      image: 'https://numerobook.com/images/OIG-19.jpg',
-      mainEntityOfPage: 'https://numerobook.com',
-    }),
-  }}
-/>
-
-
-
       <Header />
   
       <main>
@@ -65,28 +38,9 @@ export default function Page() {
 
         {/* <Features /> */}
          <USAb /> 
-           {/* <Section
-          leftHalf={<Creator />}
-          rightHalf={
-            <div className="flex flex-col justify-end">
-             <h2 className="text-2xl sm:text-3xl md:text-4xl lg:text-5xl text-gray-900 dark:text-white mb-4">
-  Our Ai will help you scale your brand
-</h2>
-<p className="text-xl font-light">
-  Numerobook is designed to make crypto easy, fast, and accessible for everyone. 
-</p>
-            </div>
-          }
-        /> */}
+
         <PredictableHero/>
-        {/* <Indiab /> */}
-        {/* <Argentinabuynow/>
-
-        <MexicoB />  */}
-{/* <UBuy/>
-
-<Nigeriab />
-<GHbuy/> */}
+     
 
    
   <Section
