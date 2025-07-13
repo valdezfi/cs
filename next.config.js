265097896f4006d40b0157c2006294f3bab349f7
valdezfi <joswellv@outlook.com>
2025-07-12 23:55:30 -0400

    fix

--- next.config.js
+++ next.config.js
@@ -1,18 +1,13 @@
 /** @type {import('next').NextConfig} */
-const path = require('path');
-
 const nextConfig = {
-  reactStrictMode: true,
-  images: {
-    domains: [], // Only needed if using remote (CDN) images
-  },
   webpack(config) {
+    // Add support for loading .md files as raw content
     config.module.rules.push({
       test: /\.md$/,
-      type: 'asset/source',
-      include: path.resolve(__dirname, 'content'), // optional
+      type: 'asset/source', // Use Webpack's asset module for raw file content
     });
-    return config;
+
+    return config;  // Don't forget to return the modified config
   },
 };
 
