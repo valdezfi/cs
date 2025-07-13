10ddec64f478c151a7a070790437a24c6e60595c
valdezfi <joswellv@outlook.com>
2025-07-12 21:33:22 -0400

    deleted some stuff

--- app/page.tsx
+++ app/page.tsx
@@ -8,7 +8,6 @@ import Accordion from "components/Accordion";
 
 import USAb from "components/USAbuy";
 import { Metadata } from 'next';
-import Script from 'next/script';
 import BrandShowcase from "components/Brandshowcase";
 import Creator from "components/ContentCreator"; // adjust the path based on your folder structure
 import PredictableHero from "components/Predictable"; // adjust the path based on your folder structure
