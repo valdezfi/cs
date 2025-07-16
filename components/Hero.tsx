90a2036602395ce9ba4716be7f3bc1e550fe3d4d
valdezfi <joswellv@outlook.com>
2025-07-15 20:13:05 -0400

    Fix the landing page

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -29,8 +29,7 @@ export default function Hero() {
   return (
     <section className="text-center my-32 mx-4 sm:mx-8 md:mx-16 lg:mx-24 bg-white dark:bg-black">
       <h1 className="font-sans text-3xl tracking-tighter sm:text-5xl md:text-6xl lg:text-7xl dark:text-white mb-6">
-        Fueling the Creator Economy with AI-Driven Products
-      </h1>
+ Fueling the Creator Economy — Connecting Brands and Agencies with Top Creators. Powered by AI.      </h1>
       <p className="text-xl text-gray-600 dark:text-gray-300 mb-12 font-light max-w-4xl mx-auto">
         We help brands grow by connecting them with top creators: UGC, Influencers, and Podcast with the support of AI.
         Whether you're launching high-performing paid ads or scaling organically,
