dd251c7369a0e776bf3235335253b48729ccbd88
valdezfi <joswellv@outlook.com>
2025-08-28 19:42:13 -0400

    Collab With UGC Creators And Influencers — made easier with AI.     </h1>

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -29,7 +29,7 @@ export default function Hero() {
   return (
     <section className="text-center my-32 mx-4 sm:mx-8 md:mx-16 lg:mx-24 bg-white dark:bg-black">
       <h1 className="font-sans text-3xl tracking-tighter sm:text-5xl md:text-6xl lg:text-7xl dark:text-white mb-6">
-Collab With The Best UGC Creators And Influencers — made easier with AI.     </h1>
+Collab With UGC Creators And Influencers — made easier with AI.     </h1>
       <p className="text-xl text-gray-600 dark:text-gray-300 mb-12 font-light max-w-4xl mx-auto">
         We help Brands and Agencies grow by connecting them with top creators: Influencers,UGC and Podcast Creators with the support of AI.
         Whether you're launching high-performing paid ads or scaling organically,
