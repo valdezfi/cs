14c0013bbd0ca69ab1f53e04859778b1c96e3749
valdezfi <joswellv@outlook.com>
2025-08-28 20:40:58 -0400

    fixed info

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -1,26 +1,4 @@
-// import Link from "next/link";
 
-// export default function Hero() {
-//   return (
-//     <section className="text-center my-32 mx-4 sm:mx-8 md:mx-16 lg:mx-24 bg-white dark:bg-black">
-//       <h1 className="font-sans text-3xl tracking-tighter sm:text-5xl md:text-6xl lg:text-7xl dark:text-white mb-6">
-//       Ads and Social Content—Boosted by UGC Creators, Influencers, and Podcasters.      </h1>
-//       <p className="text-xl text-gray-600 dark:text-gray-300 mb-12 font-light">
-//   We connect brands with creators to run high-performing ads or grow organically on social media—powered by AI. 
-//   Our AI Head of Marketing delivers top-tier marketing performance—without the six-figure salary or hassle of unreliable freelancers.
-// </p>
-
-//       <div className="flex justify-center space-x-6">
-//         <Link
-//           href="/signup"
-//           className="bg-black text-white dark:bg-white dark:text-black px-5 py-2 rounded-md text-base font-semibold hover:bg-gray-800 dark:hover:bg-gray-200 transition duration-300"
-//         >
-//           Start Your Campaign
-//         </Link>
-//       </div>
-//     </section>
-//   );
-// }
 
 
 import Link from "next/link";
