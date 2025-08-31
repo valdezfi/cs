277c0e8407872e382c67dd194be11ac6710cf1bc
valdezfi <joswellv@outlook.com>
2025-08-30 20:50:54 -0400

    fix blue

--- components/Ai.tsx
+++ components/Ai.tsx
@@ -3,9 +3,11 @@ import Link from "next/link";
 export default function AiHero() {
   return (
     <section className="text-center my-20 mx-4 sm:mx-8 md:mx-16 lg:mx-24 bg-white dark:bg-black">
-      <h1 className="font-extrabold text-4xl sm:text-5xl md:text-6xl lg:text-7xl mb-12 bg-gradient-to-r from-purple-500 via-pink-500 to-orange-400 bg-clip-text text-transparent tracking-tight">
-        AI Marketing CoPilot
-      </h1>
+     <h1 className="font-extrabold text-4xl sm:text-5xl md:text-6xl lg:text-7xl mb-12
+  bg-gradient-to-r from-blue-500 via-blue-400 to-blue-300 bg-clip-text text-transparent tracking-tight">
+  Ai Marketing CoPilot
+</h1>
+
       <p className="text-xl text-gray-600 dark:text-gray-300 mb-12 font-light max-w-4xl mx-auto">
         Move faster, create smarter. Instantly generate campaign ideas, influencer scripts, and ready-to-launch strategies.
       </p>
