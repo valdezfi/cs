97d71d4431fe79b1e3e02640f4dc2f0efe5a71c8
valdezfi <joswellv@outlook.com>
2025-09-26 21:14:15 -0400

    blog

--- lib/blogs.tsx
+++ lib/blogs.tsx
@@ -2,14 +2,14 @@
 export const blogs = [
 
   {
-    slug: "blog/affiliate-marketing-vs-influencer",
+    slug: "affiliate-marketing-vs-influencer",
     title: "Affiliate Marketing vs Influencer Marketing: What You Need to Know in 2025",
     snippet: "Understanding the difference between affiliate marketing and influencer marketing is crucial for creators, entrepreneurs, and brands looking to maximize online revenue in 2025. Both strategies can generate income, but the approach, scale, and ROI differ significantly.",
     imageUrl: "/images/blog/100.jpg",
   },
 
     {
-    slug: "blog/influencer-collaboration",
+    slug: "influencer-collaboration",
     title: "Influencer Collaboration: 10 Creative Ways to Collaborate with Influencers in 2025 (With Examples)",
     snippet: "Brands need innovative strategies to excel in influencer marketing during the year 2025. The traditional model of brand sponsorship no longer satisfies current consumers since they want genuine manufacturer-influencer partnerships which generate active interest.",
     imageUrl: "/images/blog/collabo.jpg",
