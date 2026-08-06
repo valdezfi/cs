5fe8bcceca3cd0c2d0011a8d233b021f09b30d31
valdezfi <99688081+valdezfi@users.noreply.github.com>
2026-08-06 02:43:29 +0000

    update the app

--- next.config.js
+++ next.config.js
@@ -13,23 +13,8 @@
 
 // module.exports = nextConfig;
 
-
-/** @type {import('next').NextConfig} */
-const path = require('path');
-
 const nextConfig = {
-  reactStrictMode: true,
-  images: {
-    domains: [], // Only needed if using remote (CDN) images
-  },
-  webpack(config) {
-    config.module.rules.push({
-      test: /\.md$/,
-      type: 'asset/source',
-      include: path.resolve(__dirname, 'content'), // optional
-    });
-    return config;
-  },
+  output: "export",
 };
 
 module.exports = nextConfig;
